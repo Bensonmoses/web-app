34e3f22a7fa10ecb52590bb0569943c9f3b0e8e9
bensonn-moses-palparthi <bensonmoses123@outlook.com>
2025-02-04 18:09:04 -0500

    Initial commit

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-# Name of your Google Cloud Storage bucket
-BUCKET_NAME = "web-app-clone"  # Replace with your actual bucket name
-
-# Allowed file extensions for uploading
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "mp4", "mp3", "wav", "pdf", "txt", "docx", "pptx"}
