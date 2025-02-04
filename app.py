4e8ca16d360acb35ab57a5170936c51c902b7a72
Benson Moses Palaparthi <87203089+Bensonmoses@users.noreply.github.com>
2025-02-04 14:15:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Azure Blob Storage Connection String (Replace with your actual SAS connection string)
-AZURE_STORAGE_CONNECTION_STRING = "YOUR_AZURE_STORAGE_CONNECTION_STRING"
+AZURE_STORAGE_CONNECTION_STRING = "BlobEndpoint=https://aiclonefiles.blob.core.windows.net/;QueueEndpoint=https://aiclonefiles.queue.core.windows.net/;FileEndpoint=https://aiclonefiles.file.core.windows.net/;TableEndpoint=https://aiclonefiles.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-05-01T02:08:30Z&st=2025-02-04T19:08:30Z&spr=https&sig=OvBpGJIa1fe3DdAwK9Gk7oa4pPk5i%2B2wrfjx%2F7Mbx6s%3D"
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
 # Allowed containers in Azure Blob Storage
